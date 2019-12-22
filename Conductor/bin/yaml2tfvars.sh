268d80735b7bb23cf226ff786f0d054e8d5dc33c
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-22 17:54:42 -0500

    Cleanup Conductor/bin/

--- Conductor/bin/yaml2tfvars.sh
+++ Conductor/bin/yaml2tfvars.sh
@@ -18,6 +18,7 @@ _check_directory ()
 
 cd ../..
 BASE_DIR=`pwd`
+TEMPLATE_DIR="$BASE_DIR/Terraform/conf"
 for DIR in Conductor Packer Source Terraform
 do
   _check_directory $DIR
@@ -36,7 +37,8 @@ do
   BUILDER=$BASE_DIR/Conductor/bin/yaml2tfvars_$RESOURCE_TYPE.py
   cd $DIR_PATH
   echo "INFO: Building yaml to tfvars - $BASE_DIR/$YAML_FILE"
-  $BUILDER --buildfile $FILE_NAME --templatefile Network.tfvars.TEMPLATE
+  echo "$BUILDER --buildfile $FILE_NAME --templatefile $TEMPLATE_DIR/aws_profile.TEMPLATE,$TEMPLATE_DIR/aws_vpc.TEMPLATE,$TEMPLATE_DIR/aws_subnet.TEMPLATE"
+  $BUILDER --buildfile $FILE_NAME --templatefile $TEMPLATE_DIR/aws_profile.TEMPLATE,$TEMPLATE_DIR/aws_vpc.tfvars.TEMPLATE,$TEMPLATE_DIR/aws_subnet.tfvars.TEMPLATE
   #$BUILDER --load $FILE_NAME
   cd $BASE_DIR
 done
