59f7ef798e7e65b81efc3288f70e7080b7ca8e55
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-22 18:32:13 -0500

    Update conductor.sh

--- Conductor/bin/conductor.sh
+++ Conductor/bin/conductor.sh
@@ -18,12 +18,26 @@ _check_directory ()
  
 
 CWD=`pwd`
-PRJ_DIR="../.."
+REPO_BASE="$CWD/../.."
 for DIR in Conductor Packer Source Terraform
 do
-  _check_directory
+  _check_directory $DIR
   _exit
 done
 
+###
+###
+###
+for YAML_FILE in `find $REPO_BASE -name "*.yaml" -print`
+do
+  echo -e "\n"
+  YAML_FILE_NAME=`basename $YAML_FILE`
+  YAML_FILE_PATH=`dirname $YAML_FILE`
+  RESOURCE_TYPE=`echo $FILE_NAME |awk -F. '{print $1}' |awk -F_ '{print $NF}'`
+  $BASE_DIR/Conductor/bin/yaml2tfvars_$RESOURCE_TYPE.sh $YAML_FILE
+done
+###
+###
+### 
 
 
