63279602f0f7ebc7d575f1b1d86b443a4bd49a3d
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-23 16:51:20 -0500

    Update git-tag.sh

--- Conductor/bin/git-tag.sh
+++ Conductor/bin/git-tag.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 ###
-#LATEST_TAG="Build_`date '+%Y%m%d%H%M%S'`_1"
+
 LATEST_TAG="`git tag -l |grep Jenkins_Build |sort -ur |head -1`"
 LATEST_TAG_COUNT=`echo $LATEST_TAG |awk -F_ '{print $4}'`
 
@@ -12,6 +12,6 @@ TAG_NAME="Jenkins_Build_`date '+%Y%m%d%H%M%S'`_$NEW_COUNT"
 
 echo "INFO: Applying new Tag: $TAG_NAME"
 
-#cd ${WORKSPACE}
-#git tag -a $TAG_NAME -m "$TAG_NAME"
-#git push --tags
+cd ${WORKSPACE}
+git tag -a $TAG_NAME -m "$TAG_NAME"
+git push --tags
