c53c7ac72227e01b3d1cb73c2370ca4d3301e9bd
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-27 16:17:56 -0500

    update

--- Terraform/scripts/AWS/Storage/S3tfstate/variables.tf
+++ Terraform/scripts/AWS/Storage/S3tfstate/variables.tf
@@ -1,5 +1,5 @@
-variable "region_name"			{ type = "string" }
-variable "s3bucket_name"		{ type = "string" }
-variable "project_name"			{ type = "string" }
+variable "region_name"		{ type = "string" }
+variable "s3bucket_name"	{ type = "string" }
+variable "project_name"		{ type = "string" }
 variable "organization_name"	{ type = "string" }
-variable "client_name"			{ type = "string" }
+variable "client_name"		{ type = "string" }
