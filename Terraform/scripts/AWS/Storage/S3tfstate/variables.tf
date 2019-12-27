ca16e8a4edf7e7af01bf1a9d36c72745f58a94ee
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-27 04:06:40 -0500

    Update variables.tf

--- Terraform/scripts/AWS/Storage/S3tfstate/variables.tf
+++ Terraform/scripts/AWS/Storage/S3tfstate/variables.tf
@@ -3,12 +3,3 @@ variable "s3bucket_name"		{ type = "string" }
 variable "project_name"			{ type = "string" }
 variable "organization_name"	{ type = "string" }
 variable "client_name"			{ type = "string" }
-
-#locals {
-#  s3_tags = {
-#    Name = "${bucket_name}"
-#    project = "Symphony"
-#    organization: "Advantasure Inc."
-#  }
-#}
-
