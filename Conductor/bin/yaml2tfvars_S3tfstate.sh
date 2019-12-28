7d0803c8cd788b95f0213d688bb39c830afaf9de
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-28 15:54:46 -0500

    Update yaml2tfvars_S3tfstate.sh

--- Conductor/bin/yaml2tfvars_S3tfstate.sh
+++ Conductor/bin/yaml2tfvars_S3tfstate.sh
@@ -43,6 +43,6 @@ do
   _exit
 done
 
-echo "Conductor/bin/yaml2tfvars_S3tfstate.py --buildfile $YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $WORKSPACE/$OUTPUTFOLDER"
-Conductor/bin/yaml2tfvars_S3tfstate.py --buildfile $YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $WORKSPACE/$OUTPUTFOLDER
+echo "Conductor/bin/yaml2tfvars_S3tfstate.py --buildfile $WORKSPACE/$YAML_FILE --templatefile $WORKSPACE/$TEMPLATE_FILES --outputfolder $WORKSPACE/$OUTPUTFOLDER"
+Conductor/bin/yaml2tfvars_S3tfstate.py --buildfile $WORKSPACE/$YAML_FILE --templatefile $WORKSPACE/$TEMPLATE_FILES --outputfolder $WORKSPACE/$OUTPUTFOLDER
 
