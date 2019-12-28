7639bfeba2ef42b1bc9a9ac47c628caf565cee2f
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-28 15:33:55 -0500

    Update yaml2tfvars_S3tfstate.sh

--- Conductor/bin/yaml2tfvars_S3tfstate.sh
+++ Conductor/bin/yaml2tfvars_S3tfstate.sh
@@ -23,19 +23,20 @@ _check_file ()
   fi
 }
 
-YAML_FILE=Source/Symphony/AHS/Storage/S3tfstate/Symphony_AHS_AWS_Storage_S3tfstate.yaml
-_check_file $YAML_FILE
-_exit
-
 for DIR in Conductor Packer Source Terraform
 do
   _check_directory $DIR
   _exit
 done
 
+YAML_FILE=Source/Symphony/AHS/Storage/S3tfstate/Symphony_AHS_AWS_Storage_S3tfstate.yaml
+_check_file $YAML_FILE
+_exit
+
 TEMPLATE_PATH=Terraform/conf
 OUTPUTFOLDER=Terraform/work
 
+echo "Conductor/bin/yaml2tfvars_S3tfstate.py --buildfile $YAML_FILE --templatefile $TEMPLATE_PATH/aws_profile.TEMPLATE,$TEMPLATE_PATH/aws_s3tfstate.tfvars.TEMPLATE --outputfolder $OUTPUTFOLDER"
 Conductor/bin/yaml2tfvars_S3tfstate.py --buildfile $YAML_FILE --templatefile $TEMPLATE_PATH/aws_profile.TEMPLATE,$TEMPLATE_PATH/aws_s3tfstate.tfvars.TEMPLATE --outputfolder $OUTPUTFOLDER
 
 #git commit -m "Build update yaml" $YAML_FILE
