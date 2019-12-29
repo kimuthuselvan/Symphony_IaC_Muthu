b5008ddc19c9bd415a267eb2720359cb60a6570c
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-29 06:37:33 -0500

    Update yaml2tfvars_S3tfstate.py

--- Conductor/bin/yaml2tfvars_S3tfstate.py
+++ Conductor/bin/yaml2tfvars_S3tfstate.py
@@ -1,20 +1,26 @@
 #!/usr/local/bin/python3.8
 
 import ruamel.yaml
+from string import Template
 import os
 import argparse
 import sys
-
 
 # Usage Help
 #yaml2tfvars.py --buildfile xyz.yaml --templatefile xyz.TEMPLATE ==> Build for configuration (nothing but preparing the directory structure and terraform variable files)
 
 parser = argparse.ArgumentParser(description='This is a aws account validator script.')
-parser.add_argument('--buildfile', help='Build configuration for the given yaml file')
+parser.add_argument('--yamlfile', help='Build configuration for the given yaml file')
 parser.add_argument('--templatefile', help='Template file creation for the given template file')
 parser.add_argument('--outputfolder', help='Destination folder for the given template file')
 parser.add_argument('--load',help='Load the Yaml file')
 
+# Return the count of elements in the list
+def getElementCount(element):
+    count = 0
+    if isinstance(element, list):
+        count += len(element) 
+    return count  
 # Check if the given directory is already exist in the given path, if it is not exist create it.
 def createDirectory(path):
     try:
@@ -24,62 +30,13 @@ def createDirectory(path):
             os.mkdir(path)
     except OSError:
         print ("Creation of the directory %s failed" % path)
-
-# Return the count of elements in the list
-def getElementCount(element):
-    count = 0
-    if isinstance(element, list):
-        count += len(element) 
-    return count    
-
-# Create a new file and write the config text in it.
-def createNewFile(file_path):
-    file_object = open(file_path, 'w+')
-    return file_object
-
-# Create aws profile file.
-def createAwsProfileFile(profile, template_file, s3Path, region_name, resource_name):
-    for template in template_file:
-        print("template==",template)
-        if profile.lower() in template.lower():
-            output_template_file = os.path.splitext(os.path.basename(template))[0]
-            output_template_path = s3Path + '/' + output_template_file
-            print("output_template_path==",output_template_path)
-            with open(template, "rt") as fin:
-                with open(output_template_path, "wt") as fout:
-                    for line in fin:
-                        line = line.replace('$REGION_NAME', region_name)
-                        line = line.replace('$S3BUCKET_NAME', resource_name)
-                        fout.write(line)                    
-            break
-    fin.close()
-    fout.close()
-    return output_template_path
-
-def createAwsS3TemplateFile(resource, template_file, s3Path, region_name, resource_name):
-    print("template file in s3==",template_file)
-    for template in template_file:
-        print("template s3==",template)
-        output_template_file = os.path.splitext(os.path.basename(template))[0]
-        print("output_template_file===",output_template_file)
-        output_template_path =  s3Path + '/' + output_template_file
-        print(output_template_path)
-        with open(template, "rt") as fin:
-            with open(output_template_path, "wt") as fout:
-                for line in fin:
-                    line = line.replace('$REGION_NAME', region_name)
-                    line = line.replace('$S3BUCKET_NAME', resource_name)
-                    #line = line.replace('$VPC_CIDR', cidr)
-                    fout.write(line)
-    fin.close()
-    fout.close()
-    return output_template_path
-
-# Create Build for configuration.
-def createBuildFile(yaml_file, template_file, output_folder):
-    # Load the yaml file data into dictionary
+def createBuildFile(yaml_file, templatefiles, output_folder):
+    #yaml_file = "Symphony_AHS_AWS_Network_Network.yaml"
     aws_accounts = ruamel.yaml.round_trip_load(open(yaml_file), preserve_quotes=True)
-    print("INFO:filename=",os.path.basename(yaml_file).split(".")[0])
+    count = getElementCount(aws_accounts['Region'])
+    #templatefiles = ['aws_S3tfstate.tfvars.TEMPLATE']
+    output_template_path = ""
+    #output_folder = "tmp"
     filenamewithoutext = os.path.basename(yaml_file).split(".")[0]
     filenamearr = filenamewithoutext.split("_")
     project = filenamearr[0]
@@ -87,50 +44,65 @@ def createBuildFile(yaml_file, template_file, output_folder):
     provider = filenamearr[2]
     service = filenamearr[3]
     resource_type = filenamearr[4]
-    try:  
-        baseDirPath = os.environ["WORKSPACE"]
-        #baseDirPath = 'E:\Muthu'
-    except KeyError: 
-        print("Please set the environment variable WORKSPACE")
-        sys.exit(1)
     createDirectory(output_folder)
-    print("output_folder==",output_folder)
-    count = getElementCount(aws_accounts['Region'])
+    reg_keys = {}
     buildCount = 0
-    # Region node trace
     for i in range(count):
         regionName = aws_accounts['Region'][i]['Name']
         regionPath = output_folder + '/' + regionName
-        createDirectory(regionPath)
+        createDirectory(regionPath) 
         service_path = regionPath + '/' + service
         createDirectory(service_path)
         resource_type_path = service_path +  '/' + resource_type
         createDirectory(resource_type_path)
+        for reg_elm in aws_accounts['Region'][i]:
+            if(reg_elm.lower() != "S3tfstate"):
+                region_keys = "\'REGION_"+reg_elm.upper()+"\':\'"+ str(aws_accounts['Region'][i][reg_elm])+"\',"
+                reg_keys["REGION_"+reg_elm.upper()] =  str(aws_accounts['Region'][i][reg_elm])
         count = getElementCount(aws_accounts['Region'][i]['S3tfstate'])
-     # S3tfstate node trace
+    		# S3TFSTATE node trace
         for j in range(count):
-            resource = 's3'
-            profile = 'profile'
-            s3Name = aws_accounts['Region'][i]['S3tfstate'][j]['Name']
+            s3tfstate_keys ={}
+            s3Name = str(aws_accounts['Region'][i]['S3tfstate'][j]['Name'])
             s3Name = s3Name.replace("{Region.Name}", regionName)
-            s3Path = resource_type_path + '/' + s3Name            
+            s3_dirPath = resource_type_path +"/"+ s3Name
             if(str(aws_accounts['Region'][i]['S3tfstate'][j]['Deploy']).casefold() == str(True).casefold() and str(aws_accounts['Region'][i]['S3tfstate'][j]['Terraform']).lower() == "Deploy".lower()):
                 try:
-                    createDirectory(s3Path)
-                    awsprofilepath = createAwsProfileFile(profile, template_file, s3Path, regionName, s3Name)
-                    s3profilepath = createAwsS3TemplateFile(resource, template_file, s3Path, regionName, s3Name)
-                    if(awsprofilepath !="" and s3profilepath !=""):
-                       print("INFO: Generating S3: " + s3Name + " configuration ... Done")
-                       buildCount += 1
-                       print(awsprofilepath)
-                       print(s3profilepath)
+                    buildCount += 1
+                    createDirectory(s3_dirPath)
+                    for s3_elem in aws_accounts['Region'][i]['S3tfstate'][j]:
+                        if(s3_elem.lower() != "subnet"):
+                            #s3_keys1 = '\"S3TFSTATE_'+s3_elem.upper()+"\":\""+ str(aws_accounts['Region'][i]['S3tfstate'][j][s3_elem])+"\","
+                            s3tfstate_keys["S3TFSTATE_"+s3_elem.upper()] =  str(aws_accounts['Region'][i]['S3tfstate'][j][s3_elem])
+                    s3_filenames = []
+                    for tmp_file in templatefiles:
+                        with open(tmp_file, "rt") as fin:
+                            src = fin.read()
+                            src = Template(src).safe_substitute(s3tfstate_keys)
+                            src = Template(src).safe_substitute(reg_keys)
+                            output_template_path = os.path.splitext(os.path.basename(tmp_file))[0]
+                            s3_template_path = s3_dirPath +"/"+ output_template_path
+                            #print("filename S3tfstate==",s3_template_path)
+                            with open(s3_template_path, "wt") as fout:
+                                fout.write(src)
+                            fout.close()
+                            s3_filenames.append(s3_template_path)
+                        fin.close()
+                    print("\nINFO: Generating S3tfstate: " + s3Name + " configuration ... Done")
+                    for filename in s3_filenames:
+                        print(filename)
                 except:
-                    print("ERROR: Generating S3: " + s3Name + " configuration ... Failed")
+                    print("ERROR: Generating S3tfstate: " + s3Name + " configuration ... Failed")
                 aws_accounts['Region'][i]['S3tfstate'][j]['Deploy'] = False
+    					#tfvar
+            else:
+                createDirectory(s3_dirPath)
+                for s3_elem in aws_accounts['Region'][i]['S3tfstate'][j]:
+                    if(s3_elem.lower().strip() == "name"):
+                        s3_keys["S3TFSTATE_"+s3_elem.upper()] =  str(aws_accounts['Region'][i]['S3tfstate'][j][s3_elem])            
     if(buildCount == 0):
-        print('Build configurations are in "false" status')
+        print('INFO:Build configurations are in "false" status')
     return aws_accounts
-
 #Update the Build flag in the Original Yaml file
 def updateYamlFile(aws_updated, file_path):
     with open(file_path, 'w') as yaml_file:
@@ -139,9 +111,9 @@ def updateYamlFile(aws_updated, file_path):
 # Execution starts from here.
 def main():
     args = parser.parse_args()
-    if(args.buildfile and args.templatefile):
+    if(args.yamlfile and args.templatefile):
         #Build for configuration (Preparing the directory structure and terraform variable files) 
-        buildfile = args.buildfile
+        buildfile = args.yamlfile
         template_file = args.templatefile.split(',')
         output_folder = args.outputfolder
         try:
