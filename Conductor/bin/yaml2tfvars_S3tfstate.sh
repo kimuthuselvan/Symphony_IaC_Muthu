aa77e6e0feb025c0470a22c7f82d8c4afe32ddd4
Muthuselvan <kimuthuselvan@gmail.com>
2019-12-29 06:42:31 -0500

    Update yaml2tfvars_S3tfstate.sh

--- Conductor/bin/yaml2tfvars_S3tfstate.sh
+++ Conductor/bin/yaml2tfvars_S3tfstate.sh
@@ -29,34 +29,24 @@ do
   _exit
 done
 
-YAML_FILE=Source/Symphony/AHS/Storage/S3tfstate/Symphony_AHS_AWS_Storage_S3tfstate.yaml
+YAML_FILE=$WORKSPACE/Source/Symphony/AHS/Storage/S3tfstate/Symphony_AHS_AWS_Storage_S3tfstate.yaml
 _check_file $YAML_FILE
 _exit
 
-TEMPLATE_PATH=Terraform/conf
-OUTPUTFOLDER=Terraform/work
+TEMPLATE_PATH=$WORKSPACE/Terraform/conf
+OUTPUTFOLDER=$WORKSPACE/Terraform/work
 
-TEMPLATE_FILES=$WORKSPACE/$TEMPLATE_PATH/aws_profile.TEMPLATE,$WORKSPACE/$TEMPLATE_PATH/aws_S3tfstate.tfvars.TEMPLATE
+TEMPLATE_FILES=$TEMPLATE_PATH/aws_profile.TEMPLATE,$TEMPLATE_PATH/aws_S3tfstate.tfvars.TEMPLATE
 for TFILE in `echo $TEMPLATE_FILES | sed "s/,/ /g"`
 do
   _check_file $TFILE
   _exit
 done
 
-echo -e "\nConductor/bin/yaml2tfvars_S3tfstate.py --buildfile $WORKSPACE/$YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $WORKSPACE/$OUTPUTFOLDER\n"
-Conductor/bin/yaml2tfvars_S3tfstate.py --buildfile $WORKSPACE/$YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $WORKSPACE/$OUTPUTFOLDER
+echo -e "\n$WORKSPACE/Conductor/bin/yaml2tfvars_S3tfstate.py --yamlfile $YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $OUTPUTFOLDER\n"
+$WORKSPACE/Conductor/bin/yaml2tfvars_S3tfstate.py --yamlfile $YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $OUTPUTFOLDER
 
 find . -name "*.tfvars" -print
 
-#find $OUTPUTFOLDER -name "*.tfvars" -print >$WORKSPACE/tfvars_file_list.txt
-#AWS_RESOURCE_S3TFSTATE=`cat $WORKSPACE/tfvars_file_list.txt |grep 'aws_s3tfstate.tfvars'`
-#AWS_RESOURCE_VPCTFSTATE=`cat $WORKSPACE/tfvars_file_list.txt |grep 'aws_vpc.tfvars'`
-#AWS_RESOURCE_SUBNETSTATE=`cat $WORKSPACE/tfvars_file_list.txt |grep 'aws_subnet.tfvars'`
-#for AWSRESOURCE in $AWS_RESOURCE_S3TFSTATE $AWS_RESOURCE_VPCTFSTATE $AWS_RESOURCE_SUBNETSTATE
-#do
-#  if [ ! -z $AWSRESOURCE ]
-#  then
-#    echo "Terraform Build: $AWSRESOURCE"
-#  fi
-#done
+
 
