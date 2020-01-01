d83b4ed0e1649488a8ca4d5070b4b8230c70eed7
Muthuselvan I. Kangeya <kimuthuselvan@gmail.com>
2020-01-01 16:57:35 +0000

    Update terraform_s3tfstate.sh

--- Terraform/bin/terraform_s3tfstate.sh
+++ Terraform/bin/terraform_s3tfstate.sh
@@ -4,9 +4,18 @@ export TF_LOG=TRACE
 export TF_LOG_PATH=${TERRAFORM_BASE}/logs/terraform.`date +'%Y%m%d%H%M%S'`.log
 export TF_PLUGIN_CACHE_DIR="$TERRAFORM_BASE/.terraform.d/plugin-cache"
 
-$TERRAFORM_PATH/terraform init
-$TERRAFORM_PATH/terraform plan
-$TERRAFORM_PATH/terraform apply
-$TERRAFORM_PATH/terraform deploy
-$TERRAFORM_PATH/terraform show
+ACTION=$1
+
+case $ACTION in
+deploy)
+  $TERRAFORM_PATH/terraform init
+  $TERRAFORM_PATH/terraform plan
+  $TERRAFORM_PATH/terraform apply
+  $TERRAFORM_PATH/terraform show
+  ;;
+destroy)
+  $TERRAFORM_PATH/terraform destroy
+  $TERRAFORM_PATH/terraform show
+  ;;
+esac
 
