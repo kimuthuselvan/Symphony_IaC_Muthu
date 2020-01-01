ee767e55adbfce2ad151439689b1f9b8d2a606d6
Muthuselvan <kimuthuselvan@gmail.com>
2020-01-01 12:46:08 -0500

    Update yaml2tfvars_S3tfstate.sh

--- Conductor/bin/yaml2tfvars_S3tfstate.sh
+++ Conductor/bin/yaml2tfvars_S3tfstate.sh
@@ -1,52 +1,111 @@
 #! /bin/bash
-###
-_exit ()
-{
-  [ $? -ne 0 ] && exit 1
+###============================================================================
+### Script name: yaml2tfvars_S3tfstate.sh
+### Script type: Bash script
+### Discription: Build and Deploy AWS Services.
+### 
+### Version: 1.0                                               Date: 2019-12-08
+### Author/Developer: Muthuselvan I. Kangeya
+### Reviewed by                                                Date:
+### Approved by                                                Date:
+###============================================================================
+### Shell Functions
+###============================================================================
+_exit () {
+[ $? -ne 0 ] && exit 1
 }
 
-_check_directory ()
-{
-  if [ ! -d $1 ]
+_status () {
+  if [ $? -eq 0 ]
   then
-    echo "ERROR: The Directory: $1 is missing."
+    echo "Success."
+    return 0
+  else
+    echo "Failed."
     return 1
   fi
 }
 
-_check_file ()
-{
-  if [ ! -f $1 ]
+_check_dir () {
+  echo -e "INFO: Checking directory: $1 ... \c"
+  if [ -d $1 ]
   then
-    echo "ERROR: The file: $1 is missing."
-	return 1
+    echo "Done."
+    return 0
+  else
+    echo "Failed."
+    return 1
   fi
 }
 
+_check_file () {
+  echo -e "INFO: Checking file: $1 ... \c"
+  if [ -f $1 ]
+  then
+    echo "Done."
+    return 0
+  else
+    echo "Failed."
+    return 1
+  fi
+}
+
+_draw_line () {
+L="0"
+while [ $L -lt 80 ]
+  do
+    echo -e "=\c"
+    L=$[$L+1]
+  done
+echo ""
+}
+
+###============================================================================
+### Validation
+###============================================================================
+echo "Current Working Directory: `pwd`"
 for DIR in Conductor Packer Source Terraform
 do
-  _check_directory $DIR
+  _check_dir $DIR
   _exit
 done
 
-YAML_FILE=$WORKSPACE/Source/Symphony/AHS/Storage/S3tfstate/Symphony_AHS_AWS_Storage_S3tfstate.yaml
+###============================================================================
+### YAML to tfvars process
+###============================================================================
+CONDUCTOR_BASE=$WORKSPACE/Conductor
+SOURCE_BASE=$WORKSPACE/Source
+TERRAFORM_BASE=$WORKSPACE/Terraform
+
+
+TEMPLATE_PATH=$TERRAFORM_BASE/conf
+OUTPUTFOLDER=$TERRAFORM_BASE/work
+
+YAML_PATH=$SOURCE_BASE/Symphony/AHS/Storage/S3tfstate
+YAML_FILE=$YAML_PATH/Symphony_AHS_AWS_Storage_S3tfstate.yaml
 _check_file $YAML_FILE
 _exit
 
-TEMPLATE_PATH=$WORKSPACE/Terraform/conf
-OUTPUTFOLDER=$WORKSPACE/Terraform/work
+TEMPLATE_FILES="$TEMPLATE_PATH/aws_profile.TEMPLATE \
+                $TEMPLATE_PATH/aws_S3tfstate.tfvars.TEMPLATE"
+#for TFILE in `echo $TEMPLATE_FILES | sed "s/,/ /g"`
+#do
+#  _check_file $TFILE
+#  _exit
+#done
 
-TEMPLATE_FILES=$TEMPLATE_PATH/aws_profile.TEMPLATE,$TEMPLATE_PATH/aws_S3tfstate.tfvars.TEMPLATE
-for TFILE in `echo $TEMPLATE_FILES | sed "s/,/ /g"`
-do
-  _check_file $TFILE
-  _exit
-done
+echo -e "\n$CONDUCTOR_BASE/bin/yaml2tfvars_S3tfstate.py \
+  --yamlfile $YAML_FILE \
+  --templatefile $TEMPLATE_FILES \
+  --outputfolder $OUTPUTFOLDER\n"
 
-echo -e "\n$WORKSPACE/Conductor/bin/yaml2tfvars_S3tfstate.py --yamlfile $YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $OUTPUTFOLDER\n"
-$WORKSPACE/Conductor/bin/yaml2tfvars_S3tfstate.py --yamlfile $YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $OUTPUTFOLDER
+$CONDUCTOR_BASE/bin/yaml2tfvars_S3tfstate.py \
+  --yamlfile $YAML_FILE \
+  --templatefile $TEMPLATE_FILES \
+  --outputfolder $OUTPUTFOLDER
 
 find . -name "*.tfvars" -print
 
-
-
+###============================================================================
+### End
+###============================================================================
