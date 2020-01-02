5c7f877686cb575000e6affde0d19ddcc3a28af5
vsmadhangi <vsmadhangi@gmail.com>
2020-01-01 23:06:21 -0500

    Update yaml2tfvars_EC2.py

--- Conductor/bin/yaml2tfvars_EC2.py
+++ Conductor/bin/yaml2tfvars_EC2.py
@@ -1,20 +1,26 @@
-#!/bin/python3
+#!/usr/local/bin/python3
 
 import ruamel.yaml
+from string import Template
 import os
 import argparse
 import sys
-
 
 # Usage Help
 #yaml2tfvars.py --buildfile xyz.yaml --templatefile xyz.TEMPLATE ==> Build for configuration (nothing but preparing the directory structure and terraform variable files)
 
 parser = argparse.ArgumentParser(description='This is a aws account validator script.')
-parser.add_argument('--buildfile', help='Build configuration for the given yaml file')
+parser.add_argument('--yamlfile', help='Build configuration for the given yaml file')
 parser.add_argument('--templatefile', help='Template file creation for the given template file')
 parser.add_argument('--outputfolder', help='Destination folder for the given template file')
 parser.add_argument('--load',help='Load the Yaml file')
 
+# Return the count of elements in the list
+def getElementCount(element):
+    count = 0
+    if isinstance(element, list):
+        count += len(element) 
+    return count  
 # Check if the given directory is already exist in the given path, if it is not exist create it.
 def createDirectory(path):
     try:
@@ -24,71 +30,13 @@ def createDirectory(path):
             os.mkdir(path)
     except OSError:
         print ("Creation of the directory %s failed" % path)
-
-# Return the count of elements in the list
-def getElementCount(element):
-    count = 0
-    if isinstance(element, list):
-        count += len(element) 
-    return count    
-
-# Create a new file and write the config text in it.
-def createNewFile(file_path):
-    file_object = open(file_path, 'w+')
-    return file_object
-
-# Create aws profile file.
-def createAwsProfileFile(profile, template_file, ec2Path, region_name, resource_name):
-    for template in template_file:
-        if profile.lower() in template.lower():
-            output_template_file = os.path.splitext(os.path.basename(template))[0]
-            output_template_path = ec2Path + '/' + output_template_file
-            #print(output_template_path)
-            with open(template, "rt") as fin:
-                with open(output_template_path, "wt") as fout:
-                    for line in fin:
-                        line = line.replace('$REGION_NAME', region_name)
-                        line = line.replace('$EC2_NAME', resource_name)
-                        fout.write(line)
-            break
-    fin.close()
-    fout.close()
-    return output_template_path
-
-def createAwsEC2TemplateFile(resource, template_file, ec2Path, region_name, resource_name,amiid,instance_type,vpcname,subnetname):
-    for template in template_file:
-        if resource.lower() in template.lower():
-            output_template_file = os.path.splitext(os.path.basename(template))[0]
-            output_template_path = ec2Path + '/' + output_template_file
-            #print(output_template_path)
-            with open(template, "rt") as fin:
-                with open(output_template_path, "wt") as fout:
-                    for line in fin:
-                        line = line.replace('$REGION_NAME', region_name)
-                        line = line.replace('$EC2_NAME', resource_name)
-                        line = line.replace('$AMI_ID', amiid)
-                        line = line.replace('$INSTANCE_TYPE', instance_type)
-                        line = line.replace('$KEYPAIR_NAME', resource_name)
-                        line = line.replace('$VPC_NAME', vpcname)
-                        line = line.replace('$SUBNET_NAME', subnetname)
-                        fout.write(line)
-            break
-    fin.close()
-    fout.close()
-    return output_template_path
-
-
-# Create Build for configuration.
-def createBuildFile(yaml_file, template_file, output_folder):
-    # Load the yaml file data into dictionary
+def createBuildFile(yaml_file, templatefiles, output_folder):
+    #yaml_file = "Symphony_AHS_AWS_Network_Network.yaml"
     aws_accounts = ruamel.yaml.round_trip_load(open(yaml_file), preserve_quotes=True)
-    try:  
-        baseDirPath = os.environ["WORKSPACE"]
-        #baseDirPath = 'E:\Muthu'
-    except KeyError: 
-        print("Please set the environment variable WORKSPACE")
-        sys.exit(1)
-    print("INFO:filename=",os.path.basename(yaml_file).split(".")[0])
+    count = getElementCount(aws_accounts['Region'])
+    #templatefiles = ['aws_EC2tfstate.tfvars.TEMPLATE']
+    output_template_path = ""
+    #output_folder = "tmp"
     filenamewithoutext = os.path.basename(yaml_file).split(".")[0]
     filenamearr = filenamewithoutext.split("_")
     project = filenamearr[0]
@@ -97,45 +45,64 @@ def createBuildFile(yaml_file, template_file, output_folder):
     service = filenamearr[3]
     resource_type = filenamearr[4]
     createDirectory(output_folder)
-    count = getElementCount(aws_accounts['Region'])
+    reg_keys = {}
     buildCount = 0
-    # Region node trace
     for i in range(count):
         regionName = aws_accounts['Region'][i]['Name']
         regionPath = output_folder + '/' + regionName
-        createDirectory(regionPath)
+        createDirectory(regionPath) 
         service_path = regionPath + '/' + service
         createDirectory(service_path)
         resource_type_path = service_path +  '/' + resource_type
         createDirectory(resource_type_path)
+        for reg_elm in aws_accounts['Region'][i]:
+            if(reg_elm.lower() != "EC2"):
+                region_keys = "\'REGION_"+reg_elm.upper()+"\':\'"+ str(aws_accounts['Region'][i][reg_elm])+"\',"
+                reg_keys["REGION_"+reg_elm.upper()] =  str(aws_accounts['Region'][i][reg_elm])
         count = getElementCount(aws_accounts['Region'][i]['EC2'])
-     # EC2 node trace
+    		# EC2 node trace
         for j in range(count):
-            resource = 'ec2'
-            profile = 'profile'
-            ec2Name = aws_accounts['Region'][i]['EC2'][j]['Name']
-            ec2Path = resource_type_path + '/' + ec2Name
+            ec2_keys ={}
+            ec2Name = str(aws_accounts['Region'][i]['EC2'][j]['Name'])
+            ec2Name = ec2Name.replace("{Region.Name}", regionName)
+            ec2_dirPath = resource_type_path +"/"+ ec2Name
             if(str(aws_accounts['Region'][i]['EC2'][j]['Deploy']).casefold() == str(True).casefold() and str(aws_accounts['Region'][i]['EC2'][j]['Terraform']).lower() == "Deploy".lower()):
-                try:            
-                    createDirectory(ec2Path)
-                    awsprofilepath = createAwsProfileFile(profile, template_file, ec2Path, regionName, ec2Name)
-                    amiid = str(aws_accounts['Region'][i]['EC2'][j]['AMIID'])
-                    instance_type = str(aws_accounts['Region'][i]['EC2'][j]['InstanceType'])                    
-                    vpcname = str(aws_accounts['Region'][i]['EC2'][j]['VPCName'])
-                    subnetname = str(aws_accounts['Region'][i]['EC2'][j]['SubnetName'])
-                    ec2profilepath = createAwsEC2TemplateFile(resource, template_file, ec2Path, regionName, ec2Name, amiid, instance_type, vpcname, subnetname)
-                    if(awsprofilepath !="" and ec2profilepath !=""):
-                       print("INFO: Generating EC2: " + ec2Name + " configuration ... Done")
-                       buildCount += 1
-                       print(awsprofilepath)
-                       print(ec2profilepath)
+                try:
+                    buildCount += 1
+                    createDirectory(ec2_dirPath)
+                    for ec2_elem in aws_accounts['Region'][i]['EC2'][j]:
+                        if(ec2_elem.lower() != "subnet"):
+                            #ec2_keys1 = '\"EC2_'+ec2_elem.upper()+"\":\""+ str(aws_accounts['Region'][i]['EC2'][j][ec2_elem])+"\","
+                            ec2_keys["EC2_"+ec2_elem.upper()] =  str(aws_accounts['Region'][i]['EC2'][j][ec2_elem])
+                    ec2_filenames = []
+                    for tmp_file in templatefiles:
+                        with open(tmp_file, "rt") as fin:
+                            src = fin.read()
+                            src = Template(src).safe_substitute(ec2_keys)
+                            src = Template(src).safe_substitute(reg_keys)
+                            output_template_path = os.path.splitext(os.path.basename(tmp_file))[0]
+                            ec2_template_path = ec2_dirPath +"/"+ output_template_path
+                            #print("filename EC2==",ec2_template_path)
+                            with open(ec2_template_path, "wt") as fout:
+                                fout.write(src)
+                            fout.close()
+                            ec2_filenames.append(ec2_template_path)
+                        fin.close()
+                    print("\nINFO: Generating EC2: " + ec2Name + " configuration ... Done")
+                    for filename in ec2_filenames:
+                        print(filename)
                 except:
                     print("ERROR: Generating EC2: " + ec2Name + " configuration ... Failed")
                 aws_accounts['Region'][i]['EC2'][j]['Deploy'] = False
+    					#tfvar
+            else:
+                createDirectory(ec2_dirPath)
+                for ec2_elem in aws_accounts['Region'][i]['EC2'][j]:
+                    if(ec2_elem.lower().strip() == "name"):
+                        ec2_keys["EC2_"+ec2_elem.upper()] =  str(aws_accounts['Region'][i]['EC2'][j][ec2_elem])            
     if(buildCount == 0):
-        print('Build configurations are in "false" status')
+        print('INFO:Build configurations are in "false" status')
     return aws_accounts
-
 #Update the Build flag in the Original Yaml file
 def updateYamlFile(aws_updated, file_path):
     with open(file_path, 'w') as yaml_file:
@@ -144,9 +111,9 @@ def updateYamlFile(aws_updated, file_path):
 # Execution starts from here.
 def main():
     args = parser.parse_args()
-    if(args.buildfile and args.templatefile):
+    if(args.yamlfile and args.templatefile):
         #Build for configuration (Preparing the directory structure and terraform variable files) 
-        buildfile = args.buildfile
+        buildfile = args.yamlfile
         template_file = args.templatefile.split(',')
         output_folder = args.outputfolder
         try:
