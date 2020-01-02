48aad3040cd691738436e0f3fe70ac4ff9f50156
kimuthuselvan <32721869+kimuthuselvan@users.noreply.github.com>
2020-01-02 02:06:44 -0500

    Script update

--- Conductor/bin/git-commit.sh
+++ Conductor/bin/git-commit.sh
@@ -2,15 +2,4 @@
 ###
 echo "INFO: Git commit fils ..."
 cd ${WORKSPACE}
-git commit --dry-run -m "Build Number: ${BUILD_NUMBER} - Jenkins commit" -a
-#MODIFIED_FILES_LIST=`git ls-files -m`
-#if [ ! -z $MODIFIED_FILES_LIST ]
-#then
-#  echo -e "INFO: Folloing files will be commit into Git: \n $MODIFIED_FILES_LIST"
-#  git commit -m "Build Number: ${BUILD_NUMBER} - Jenkins commit" -a
-#  if [ $? -eq 0 ]
-#  then
-#    git push
-#  fi
-#fi
-
+git commit -m "Build Number: ${BUILD_NUMBER} - Jenkins commit" -a
--- Terraform/bin/terraform_S3tfstate.sh
+++ Terraform/bin/terraform_S3tfstate.sh
@@ -104,9 +104,9 @@ do
       $TERRAFORM_PATH/terraform plan -var-file=$TFVARS
       _status
       echo -e "INFO: Processing terraform apply ... \c"
-      #$TERRAFORM_PATH/terraform apply -auto-approve -var-file=$TFVARS
+      $TERRAFORM_PATH/terraform apply -auto-approve -var-file=$TFVARS
       _status
-  	  #mv terraform.tfstate $TF_BUILD_DIR/
+  	  mv -f terraform.tfstate $TF_BUILD_DIR/
     else
       echo "INFO: Nothing to do"
     fi
