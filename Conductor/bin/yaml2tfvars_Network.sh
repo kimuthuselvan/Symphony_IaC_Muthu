979ceea2d3228e61e27f4d6683a31b5be8791cd3
Muthuselvan <kimuthuselvan@gmail.com>
2020-01-02 08:00:26 -0500

    Update yaml2tfvars_Network.sh

--- Conductor/bin/yaml2tfvars_Network.sh
+++ Conductor/bin/yaml2tfvars_Network.sh
@@ -1,41 +1,122 @@
 #! /bin/bash
-###
-_exit ()
-{
+###============================================================================
+### Script name: yaml2tfvars_Network.sh
+### Script type: Bash script
+### Discription: Build and Deploy AWS Services.
+### 
+### Version: 1.0                                               Date: 2019-12-08
+### Author/Developer: Muthuselvan I. Kangeya
+### Reviewed by                                                Date:
+### Approved by                                                Date:
+###============================================================================
+### Shell Functions
+###============================================================================
+_exit () {
   [ $? -ne 0 ] && exit 1
 }
 
-_check_directory ()
-{
-  if [ ! -d $1 ]
+_status () {
+  if [ $? -eq 0 ]
   then
-    echo "ERROR: The Directory: $1 is missing."
+    echo "Success."
+  else
+    echo "Failed."
     return 1
   fi
 }
 
-_check_file ()
-{
-  if [ ! -f $1 ]
+_check_dir () {
+  echo -e "INFO: Checking directory: $1 ... \c"
+  if [ -d $1 ]
   then
-    echo "ERROR: The file: $1 is missing."
-	return 1
+    echo "Done."
+  else
+    echo "Failed."
+    return 1
   fi
 }
 
-YAML_FILE=$WORKSPACE/Source/Symphony/AHS/Network/Network/Symphony_AHS_AWS_Network_Network.yaml
-_check_file $YAML_FILE
-_exit
+_check_file () {
+  echo -e "INFO: Checking file: $1 ... \c"
+  if [ -f $1 ]
+  then
+    echo "Done."
+  else
+    echo "Failed."
+    return 1
+  fi
+}
 
-TEMPLATE_PATH=$WORKSPACE/Terraform/conf
-OUTPUTFOLDER=$WORKSPACE/Terraform/work
+_draw_line () {
+L="0"
+while [ $L -lt 80 ]
+  do
+    echo -e "=\c"
+    L=$[$L+1]
+  done
+echo ""
+}
 
-TEMPLATE_FILES=$TEMPLATE_PATH/aws_profile.TEMPLATE,$TEMPLATE_PATH/aws_vpc.tfvars.TEMPLATE,$TEMPLATE_PATH/aws_subnet.tfvars.TEMPLATE
-for TFILE in `echo $TEMPLATE_FILES | sed "s/,/ /g"`
+###============================================================================
+### Validation
+###============================================================================
+echo "Current Working Directory: `pwd`"
+for DIR in Conductor Packer Source Terraform
 do
-  _check_file $TFILE
+  _check_dir $DIR
   _exit
 done
 
-echo "$WORKSPACE/Conductor/bin/yaml2tfvars_Network.py --yamlfile $YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $OUTPUTFOLDER"
-$WORKSPACE/Conductor/bin/yaml2tfvars_Network.py --yamlfile $YAML_FILE --templatefile $TEMPLATE_FILES --outputfolder $OUTPUTFOLDER
+###============================================================================
+### YAML to tfvars process
+###============================================================================
+CONDUCTOR_BASE=Conductor
+SOURCE_BASE=Source
+TERRAFORM_BASE=Terraform
+TEMPLATE_PATH=$TERRAFORM_BASE/conf
+
+for YAML_FILE in `find $SOURCE_BASE -name "*_Network.yaml" -print`
+do
+  YAML_FILE_NAME=`basename $YAML_FILE`
+  YAML_FILE_PATH=`dirname $YAML_FILE`
+  YAML_FILE_PREFIX=`echo $YAML_FILE_NAME |awk -F. '{print $1}'`
+  echo "YAML_FILE_PREFIX:$YAML_FILE_PREFIX"
+  ADV_PROJECT=`echo $YAML_FILE_PREFIX |awk -F_ '{print $1}'`
+  echo "ADV_PROJECT=$ADV_PROJECT"
+  ADV_CLIENT=`echo $YAML_FILE_PREFIX |awk -F_ '{print $2}'`
+  echo "ADV_CLIENT=$ADV_CLIENT"
+  AWS_PROVIDER=`echo $YAML_FILE_PREFIX |awk -F_ '{print $3}'`
+  echo "AWS_PROVIDER=$AWS_PROVIDER"
+  AWS_SERVICE=`echo $YAML_FILE_PREFIX |awk -F_ '{print $4}'`
+  echo "AWS_SERVICE=$AWS_SERVICE"
+  AWS_RESOURCE=`echo $YAML_FILE_PREFIX |awk -F_ '{print $5}'`
+  echo "AWS_RESOURCE=$AWS_RESOURCE"
+     
+  OUTPUTFOLDER=$TERRAFORM_BASE/work/$ADV_PROJECT/$ADV_CLIENT
+  mkdir -p $OUTPUTFOLDER
+  
+  cp -f $SOURCE_BASE/$ADV_PROJECT/$ADV_CLIENT/aws_profile $OUTPUTFOLDER/
+    
+  _draw_line
+  echo "$YAML_FILE_PREFIX"
+  _draw_line
+  
+  TEMPLATE_FILES=$TEMPLATE_PATH/aws_vpc.tfvars.TEMPLATE,$TEMPLATE_PATH/aws_subnet.tfvars.TEMPLATE
+  
+  echo -e "\nCommand: $CONDUCTOR_BASE/bin/yaml2tfvars_S3tfstate.py \
+    --yamlfile $YAML_FILE \
+    --templatefile $TEMPLATE_FILES \
+    --outputfolder $OUTPUTFOLDER\n"
+  
+  $CONDUCTOR_BASE/bin/yaml2tfvars_S3tfstate.py \
+    --yamlfile $YAML_FILE \
+    --templatefile $TEMPLATE_FILES \
+    --outputfolder $OUTPUTFOLDER
+  
+  _draw_line
+  find $WORKSPACE -name "aws_S3tfstate.tfvars" -print >$WORKSPACE/${ADV_CLIENT}_Network.build
+done
+
+###============================================================================
+### End
+###============================================================================
