d09f8535fea2cb922014e685d6559047df50c513
Muthuselvan <kimuthuselvan@gmail.com>
2020-01-02 00:23:57 -0500

    Update push2stage.sh

--- Conductor/bin/push2stage.sh
+++ Conductor/bin/push2stage.sh
@@ -1,21 +1,19 @@
 #! /bin/bash
 ###
-
-STAGE_PATH=/awsops/WorkSpace/GitHub/Symphony_IaC_Muthu
-if [ ! -d $STAGE_PATH ]
+SOURCE_PATH=${WORKSPACE}
+TARGET_PATH=/awsops/WorkSpace/GitHub/Symphony_IaC_Muthu
+if [ ! -d $TARGET_PATH ]
 then
-  echo "ERROR: Stage path (/SecOps) is missing"
+  echo "ERROR: Stage path: $TARGET_PATH is missing"
   exit 1
 fi
 
-SOURCE_PATH=${WORKSPACE}
-TARGET_PATH=$STAGE_PATH
 rsync -v -c -r ${WORKSPACE}/* $TARGET_PATH/
 if [ $? -ne 0 ]
 then
   exit 1
 fi
-chmod -R 774 $TARGET_PATH/*
+chmod -R 744 $TARGET_PATH/*
 if [ $? -ne 0 ]
 then
   exit 1
