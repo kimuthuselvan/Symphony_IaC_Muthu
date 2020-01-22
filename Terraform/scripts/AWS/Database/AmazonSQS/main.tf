67f2c09f8b167f90acf92f93b0557eb31fa8588a
Muthuselvan I. Kangeya <kimuthuselvan@gmail.com>
2020-01-21 22:55:24 -0500

    AmazonSQS

---
+++ Terraform/scripts/AWS/Database/AmazonSQS/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "Symphony-Kansas-UAT-SQS"
+  delay_seconds             = 0
+  max_message_size          = 262144
+  message_retention_seconds = 345600
+  receive_wait_time_seconds = 0
+  #redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
+
+  tags = {
+    Name = "Symphony-Kansas-UAT-SQS"
+    Project = "Symphony"
+    Client = "Kansas"
+    Environment = "UAT-DMZ"
+  }
+}
